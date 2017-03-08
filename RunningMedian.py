9fc62c391405ddfdd38a92a3e663fc109cb8abe6
develop-build <anubhavj1357@gmail.com>
2017-03-09 03:57:06 +0530

    Code Documentation Added

--- RunningMedian.py
+++ RunningMedian.py
@@ -1,7 +1,13 @@
+# Run the program  on terminal using 'python RunningMedian.py'#
+number_input = input()# Take the number of input to be taken 
+list = []#list for storing the values which are input
 
-number_input = input()
-list = []
-
+#loop runs until the limit is reached
+#in every iteration a number is taken as input
+#and appended to the list. After appending new number, the list is sorted in ascending order
+#after sorting length of the list is calculated
+#if the length is even then mean of two middle most values is printed
+#else the middle most element in the list is printed
 for i in range(number_input):
     number = input()
     list.append(number)
